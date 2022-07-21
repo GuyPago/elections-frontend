65a0d3455bcf3f3960c5d642e62e4cd1680631e6
Guy Taggar <guypago@outlook.com>
2022-07-21 23:17:44 +0300

    Add config file

---
+++ src/config/appsettings.ts
@@ -0,0 +1,10 @@
+export const DEV_NAME = "GT";
+export const BACKEND_URL = "http://localhost:8080/api";
+export const DATABASE_ERROR_MSG = "בעיה בשרת";
+export const pagesUrl = {
+  results: "/results",
+  info: "/info",
+  about: "/about",
+  error: "/error",
+  login: "/login",
+};

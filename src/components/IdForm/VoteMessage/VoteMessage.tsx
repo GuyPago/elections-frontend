02c78f6c5f443676d206f3fefd80304fefd77aca
Guy Taggar <guypago@outlook.com>
2022-07-21 23:15:22 +0300

    Add consts files

---
+++ src/components/IdForm/VoteMessage/VoteMessage.tsx
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+import { FC } from "react";
+import HasVotedResult from "../../../enums/HasVotedResult";
+import { VOTE_INIT_VALUE } from "../IdFormConsts";
+import * as msg from "./formMessages";
+
+interface VoterProps {
+  idInput: string;
+  hasVoted: HasVotedResult;
+  isValid: boolean;
+}
+
+enum TextColor {
+  Red,
+  Green,
+}
+
+const messageCompGen = (content: string, textColor: TextColor = TextColor.Red): JSX.Element => {
+  const textStyle = classNames(
+    { "id-form--voted-warn": textColor === TextColor.Red },
+    { "id-form--voted-accept": textColor === TextColor.Green }
+  );
+
+  return <p className={textStyle}>{content}</p>;
+};
+
+export const VoteMessage: FC<VoterProps> = (props) => {
+  if (props.idInput === VOTE_INIT_VALUE) {
+    return <div />;
+  }
+
+  if (!props.idInput) {
+    return messageCompGen(msg.NO_ID_MSG);
+  }
+
+  if (!props.isValid) {
+    return messageCompGen(msg.INVALID_ID_MSG);
+  }
+
+  let component: JSX.Element;
+
+  if (props.hasVoted === HasVotedResult.Yes) {
+    component = messageCompGen(msg.ALREADY_VOTED_MSG);
+  } else if (props.hasVoted === HasVotedResult.Unknown) {
+    component = messageCompGen(msg.DB_ERROR_MSG);
+    console.log("gege");
+  } else {
+    component = messageCompGen(msg.ACCEPT_ID_MSG, TextColor.Green);
+  }
+
+  return component;
+};
---
+++ src/components/IdForm/VoteMessage/formMessages.ts
@@ -0,0 +1,5 @@
+export const NO_ID_MSG = 'לא הוכנסה ת"ז';
+export const INVALID_ID_MSG = "תעודת זהות לא תקינה";
+export const ALREADY_VOTED_MSG = "כבר הצבעת";
+export const ACCEPT_ID_MSG = "הינך זכאי להצביע";
+export const DB_ERROR_MSG = "שגיאה בשרת";

660b6accadcec5c8e3166a01cb0ba9dd42f84927
Guy Taggar <guypago@outlook.com>
2022-07-25 15:22:00 +0300

    Add results

--- src/data/navbarItemsData.ts
+++ src/data/navbarItemsData.ts
@@ -4,16 +4,21 @@ import NavbarItem from "../models/NavbarItem";
 const navbarItemsData: NavbarItem[] = [
   {
     id: "1",
+    title: "תוצאות",
+    link: pagesUrl.results,
+  },
+  {
+    id: "2",
     title: "אודות",
     link: pagesUrl.about,
   },
   {
-    id: "2",
+    id: "3",
     title: "מידע",
     link: pagesUrl.info,
   },
   {
-    id: "3",
+    id: "4",
     title: "המרכז האישי",
     link: pagesUrl.login,
   },
